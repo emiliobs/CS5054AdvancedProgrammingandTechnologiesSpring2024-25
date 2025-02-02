cb8d0cfd4d44ce7bb35593ebd52c6ae3a26a797e
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-02 20:20:55 +0000

    Created program, Gui  and Code are done and working

---
+++ AddNumber/src/addnumber/AddNumber.java
@@ -0,0 +1,18 @@
+package addnumber;
+
+import My.Addnumber.AddNumberUI;
+
+/**
+ *
+ * @author Emilio
+ */
+public class AddNumber
+{
+    public static void main(String[] args)
+    {
+        AddNumberUI    addNumberUI = new AddNumberUI();
+        addNumberUI.setLocationRelativeTo(null);
+        addNumberUI.setVisible(true);
+    }
+    
+}
