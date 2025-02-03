950811691e8e9f74f992b54119377b8f2f80a9ca
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-03 16:47:56 +0000

    Simple Calculator GUI, Week 3 - Done

---
+++ SimpleCalculatorGUI2/src/SimpleCalculatorGUI2.java
@@ -0,0 +1,18 @@
+
+import GUI.CalculatorGUI;
+
+/**
+ *
+ * @author Emilio
+ */
+public class SimpleCalculatorGUI2
+{
+
+      public static void main(String[] args)
+    {
+        CalculatorGUI calculatorGUI = new CalculatorGUI();
+        calculatorGUI.setLocationRelativeTo(null);
+        calculatorGUI.setVisible(true);
+    }
+    
+}
