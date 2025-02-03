2850f66d8bf1fe839c3a1a3fe0ac9dd1127f2d40
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-03 18:28:15 +0000

    Current Convert App Done

---
+++ ConversionWorkshop3/src/conversionworkshop3/ConversionWorkshop3.java
@@ -0,0 +1,19 @@
+package conversionworkshop3;
+
+import GUI.ConversionGUI;
+
+/**
+ *
+ * @author Emilio
+ */
+public class ConversionWorkshop3
+{
+
+   public static void main(String[] args)
+    {
+        ConversionGUI conversionGUI = new ConversionGUI();
+        conversionGUI.setLocationRelativeTo(null);
+        conversionGUI.setVisible(true);
+    }
+    
+}
--- SimpleCalculatorGUI2/src/GUI/CalculatorGUI.java
+++ SimpleCalculatorGUI2/src/GUI/CalculatorGUI.java
@@ -73,17 +73,17 @@ public class CalculatorGUI extends javax.swing.JFrame
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(137, 137, 137)
                 .addComponent(jLabel1)
-                .addGap(137, 137, 137))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(17, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
                 .addComponent(jLabel1)
-                .addGap(16, 16, 16))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         jPanel3.setBackground(new java.awt.Color(255, 0, 0));
