fef82ec27f1e7fcdc374ec2000713718c490c866
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-09 21:05:08 +0000

    Workshop 4,   Modules JTable - week 4

---
+++ JTableWeek4/src/jtableweek4/JTableWeek4.java
@@ -0,0 +1,27 @@
+package jtableweek4;
+
+
+import GUI.ModuleGUI;
+import java.io.Console;
+
+/**
+ *
+ * @author Emilio
+ */
+public class JTableWeek4
+{
+
+    
+
+    public static void main(String[] args)
+    {
+      
+        ModuleGUI moduleGUI = new ModuleGUI();
+        
+     moduleGUI.setLocationRelativeTo(null);
+     moduleGUI.setVisible(true);
+        
+        
+    }
+    
+}
